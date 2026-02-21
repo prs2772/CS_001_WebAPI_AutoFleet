cf8be08f1c203b60049a40a37896a9601258710d
ParisRamirezS <pramirezs2705@gmail.com>
2026-02-21 01:08:35 -0600

    AF-004 Added new VehicleStatus Core entity

---
+++ AutoFleet.Core/Entities.cs/VehicleStatus.cs
@@ -0,0 +1,8 @@
+namespace AutoFleet.Core.Entities;
+
+public enum VehicleStatus
+{
+    Available = 1,// Ready to Use
+    Rented = 2,// En uso
+    Maintenance = 3 // Not available due to repairs or upkeep
+}
