96122f18b55d454b6b364c4a0eb4f7ca53780686
ParisRamirezS <pramirezs2705@gmail.com>
2026-02-21 01:27:08 -0600

    AF-004 Added contact to get summary of available fleet

--- AutoFleet.Core/Interfaces/IVehicleRepository.cs
+++ AutoFleet.Core/Interfaces/IVehicleRepository.cs
@@ -4,8 +4,17 @@ namespace AutoFleet.Core.Interfaces
 {
     public interface IVehicleRepository
     {
+        #region SingleVehicle Management
         Task<IEnumerable<Vehicle>> GetAllAsync();
         Task<Vehicle?> GetByIdAsync(int id);
         Task AddAsync(Vehicle vehicle);
+
+        #endregion
+
+        #region Fleet Management
+        // 20260221 + PRS: Added contact to get summary of available fleet
+        Task<List<InventoryItemDto>> GetAvailableFleetSummaryAsync();
+
+        #endregion
     }
 }
